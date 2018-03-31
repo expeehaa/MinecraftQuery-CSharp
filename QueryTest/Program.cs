0d5b5afc549ccb7c586e16f50ff1bede4d8e305f
expeehaa <expeehaa@outlook.com>
2018-03-31 02:42:05 +0200

    Added test project

---
+++ QueryTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MinecraftQuery;
+
+namespace QueryTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var mapi = new MojangApi();
+            var xph = mapi.GetAccountInfoAsync("expeehaa").GetAwaiter().GetResult();
+            Console.WriteLine(xph.ToString());
+            var xphnames = mapi.GetAllAccountNamesAsync(xph.Uuid).GetAwaiter().GetResult();
+            Console.WriteLine(xphnames.ToString());
+            Console.ReadKey();
+        }
+    }
+}
